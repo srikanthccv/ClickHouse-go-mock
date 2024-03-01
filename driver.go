764750a728a4f0987c4947a9166386332bdbe201
Raj <rkssisodiya@gmail.com>
2024-03-01 14:59:41 +0530

    feat: Add ability to create mock clickhouse with custom query matcher

--- driver.go
+++ driver.go
@@ -36,11 +36,18 @@ func init() {
 
 // NewClickHouseNative creates clickhousemock database mock to manage expectations.
 func NewClickHouseNative(options *clickhouse.Options) (*clickhousemock, error) {
+	return NewClickHouseWithQueryMatcher(options, sqlmock.QueryMatcherEqual)
+}
+
+func NewClickHouseWithQueryMatcher(
+	options *clickhouse.Options,
+	queryMatcher sqlmock.QueryMatcher,
+) (*clickhousemock, error) {
 	clickHousePool.Lock()
 	dsn := fmt.Sprintf("clickhousemock_db_%d", clickHousePool.counter)
 	clickHousePool.counter++
 
-	cmock := &clickhousemock{dsn: dsn, drv: clickHousePool, ordered: true, queryMatcher: sqlmock.QueryMatcherEqual}
+	cmock := &clickhousemock{dsn: dsn, drv: clickHousePool, ordered: true, queryMatcher: queryMatcher}
 	clickHousePool.conns[dsn] = cmock
 	clickHousePool.Unlock()
 
